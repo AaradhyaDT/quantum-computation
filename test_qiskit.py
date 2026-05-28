d62576085ce5fc450d58072f2e1493c810db6a0b
AaradhyaDT <aaradhyadevtmr@gmail.com>
2026-05-28 17:33:25 +0545

    Initialization

---
+++ test_qiskit.py
@@ -0,0 +1,9 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+
+qc = QuantumCircuit(1, 1)
+qc.h(0)
+qc.measure(0, 0)
+
+result = AerSimulator().run(qc, shots=1000).result()
+print(result.get_counts())
